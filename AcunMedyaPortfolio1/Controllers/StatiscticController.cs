43c05eefecd10769f15cea76fc9a1741c513a70a
ENES <ENES@DESKTOP-C95HC1U>
2025-02-17 17:20:57 +0300

    AcunMedyaPortoflioCommit

---
+++ AcunMedyaPortfolio1/Controllers/StatiscticController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaPortfolio1.Models;
+
+public class StatiscticController : Controller
+{
+    DbDominicPortfolyoEntities3 db = new DbDominicPortfolyoEntities3();
+    public ActionResult Index()
+    {
+        ViewBag.categorysayisi = db.TblCategory.Count();
+        ViewBag.projesayisi = db.TblProject.Count();
+        return View();
+    }
+}
